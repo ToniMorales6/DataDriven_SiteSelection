8f95ce3b92c2ebe43d25c726c536c9f316132b7a
Toni-PC <toni.morales6@gmail.com>
2024-06-26 10:46:24 +0200

    añadir mapas actualizados

--- Número de Guarderias/competition.py
+++ Número de Guarderias/competition.py
@@ -10,55 +10,80 @@ Created on Fri Jun  7 10:46:42 2024
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
+import matplotlib.pyplot as plt
 
 import folium
 from folium.plugins import HeatMap
 
-# Load the censofilt DataFrame
 censo = pd.read_csv('censolocales.csv')
 
-# Filter for the relevant points
-censofilt = censo[censo['Nom_Local'].str.contains('guarderia|llar d\'infants|guardería', case=False, na=False)]
+nulos = censo.isnull().sum()
 
-# Load the refpoints GeoJSON file
+
+censofilt = censo[censo['Nom_Local'].str.contains('guarderia|llar d\'infants|guardería|bressol|infancia|guardería|infància|infancia|infantil|parvulario', case=False, na=False)]
+
+censofilt = censofilt[censofilt['Nom_Activitat'] == 'Ensenyament' ]
+
+
+censofilt[['Nom_Barri','Nom_Districte','Latitud', 'Longitud', 'Nom_Local']].to_csv('guarderiasfinal.csv')
+
+# Agrupar por 'Nom_Districte' y contar las ocurrencias
+districte_counts = censofilt['Nom_Districte'].value_counts().sort_index()
+
+# Crear el gráfico de barras
+plt.figure(figsize=(10, 6))
+districte_counts.plot(kind='bar', color='skyblue')
+plt.title('Número de guarderías por distrito')
+plt.xlabel('Nombre del distrito')
+plt.ylabel('total')
+plt.xticks(rotation=45)
+plt.show()
+
 refpoints = gpd.read_file('hundredpoints.geojson')
 
-# Convert the censofilt DataFrame to a GeoDataFrame
 geometry = [Point(xy) for xy in zip(censofilt['Longitud'], censofilt['Latitud'])]
 gdf_censofilt = gpd.GeoDataFrame(censofilt, geometry=geometry)
 
-# Set the same coordinate reference system (CRS) for both GeoDataFrames
 refpoints.set_crs(epsg=4326, inplace=True)
 gdf_censofilt.set_crs(epsg=4326, inplace=True)
 
-# Buffer each point in refpoints by 3 km (3000 meters)
 buffered_refpoints = refpoints.copy()
-buffered_refpoints['geometry'] = buffered_refpoints.geometry.buffer(2000 / 111000)  # Approximation for degrees
+buffered_refpoints['geometry'] = buffered_refpoints.geometry.buffer(750 / 111000)  # Approximation for degrees
 
-# Perform spatial join to count points within each buffer
 join_gdf = gpd.sjoin(gdf_censofilt, buffered_refpoints, op='within')
 
-# Count the number of points within each buffer
 counts = join_gdf.groupby('index_right').size()
 
-# Add the counts back to the original refpoints
 refpoints['count_within_2km'] = counts
 
-# Fill NaN values with 0 (for points with no nearby points within 3 km)
 refpoints['count_within_2km'] = refpoints['count_within_2km'].fillna(0).astype(int)
 
+nulos = refpoints.isnull().sum()
 
-# Find the maximum and minimum scores
+
+
 max_score = refpoints['count_within_2km'].max()
 min_score = refpoints['count_within_2km'].min()
 
-# Calculate new scores based on the maximum and minimum scores
-refpoints['new_score'] = refpoints['count_within_2km'].apply(lambda x: 20 - ((x - min_score) * (20 / (max_score - min_score))))
+refpoints['competencia_score'] = refpoints['count_within_2km'].apply(lambda x: 20 - ((x - min_score) * (20 / (max_score - min_score))))
+
+puntaje = refpoints[['geometry', 'competencia_score']]
+
+puntaje.to_csv('competitionpoints.csv')
+
+
+# Plot de resultados
+plt.figure(figsize=(8, 6))
+plt.boxplot(refpoints['competencia_score'])
+plt.title('Boxplot de Competencia Score')
+plt.ylabel('Competencia Score')
+plt.grid(True)
+plt.show()
+
 
 
 # MAPA!
 
-# Create a map centered around the average latitude and longitude
 map_center = [refpoints.geometry.y.mean(), refpoints.geometry.x.mean()]
 m = folium.Map(location=map_center, zoom_start=12)
 
@@ -66,22 +91,18 @@ m = folium.Map(location=map_center, zoom_start=12)
 for idx, row in refpoints.iterrows():
     folium.Marker(
         location=[row.geometry.y, row.geometry.x],
-        popup=f'Count within 3 km: {row["count_within_3km"]}',
-        tooltip=row["count_within_3km"]
+        popup=f'Count within 750m: {row["count_within_2km"]}',
+        tooltip=row["count_within_2km"]
     ).add_to(m)
 
-# Save the map to an HTML file
 m.save('map_with_counts.html')
 
-# Display the map
 m
 
-# Create a heat map
-heat_data = [[row.geometry.y, row.geometry.x, row['count_within_3km']] for idx, row in refpoints.iterrows()]
+heat_data = [[row.geometry.y, row.geometry.x, row['count_within_2km']] for idx, row in refpoints.iterrows()]
 m_heat = folium.Map(location=map_center, zoom_start=12)
 HeatMap(heat_data).add_to(m_heat)
 
-# Save the heat map to an HTML file
 m_heat.save('heat_map.html')
 
 # Display the heat map
@@ -89,3 +110,103 @@ m_heat
 
 
 
+
+
+
+max_count = refpoints['count_within_2km'].max()
+colormap = folium.LinearColormap(colors=['red', 'yellow', 'green'], vmin=0, vmax=max_count)
+
+map_center = [refpoints.geometry.y.mean(), refpoints.geometry.x.mean()]
+m = folium.Map(location=map_center, zoom_start=12)
+
+for idx, row in refpoints.iterrows():
+    num_count = row['count_within_2km']
+    color = colormap(num_count)
+    
+    # Añadir un círculo alrededor del punto
+    folium.Circle(
+        location=[row.geometry.y, row.geometry.x],
+        radius=750,  # Radio de 750 m
+        color=None,
+        fill=True,
+        fill_color=color,
+        fill_opacity=0.2,
+        popup=f'Count within 750m: {num_count}',
+        opacity=0
+    ).add_to(m)
+    
+    # Añadir el marcador del punto
+    folium.CircleMarker(
+        location=[row.geometry.y, row.geometry.x],
+        radius=4,
+        color='black',
+        fill=True,
+        fill_color='black',
+        fill_opacity=1,
+        popup=f'Count within 750m: {num_count}'
+    ).add_to(m)
+
+colormap.add_to(m)
+
+m.save('map_with_counts.html')
+
+m
+
+heat_data = [[row.geometry.y, row.geometry.x, row['count_within_2km']] for idx, row in refpoints.iterrows()]
+m_heat = folium.Map(location=map_center, zoom_start=12)
+HeatMap(heat_data).add_to(m_heat)
+
+m_heat.save('heat_map2.html')
+
+m_heat
+
+
+
+
+max_score = puntaje['competencia_score'].max()
+min_score = puntaje['competencia_score'].min()
+colormap = folium.LinearColormap(colors=['red', 'yellow', 'green'], vmin=min_score, vmax=max_score)
+
+map_center = [puntaje.geometry.y.mean(), puntaje.geometry.x.mean()]
+m = folium.Map(location=map_center, zoom_start=12)
+
+for idx, row in puntaje.iterrows():
+    competencia_score = row['competencia_score']
+    color = colormap(competencia_score)
+    
+    # Añadir un círculo alrededor del punto
+    folium.Circle(
+        location=[row.geometry.y, row.geometry.x],
+        radius=750,  # Radio de 750 m
+        color=None,
+        fill=True,
+        fill_color=color,
+        fill_opacity=0.2,
+        popup=f'Competencia Score: {competencia_score}',
+        opacity=0
+    ).add_to(m)
+    
+    # Añadir el marcador del punto
+    folium.CircleMarker(
+        location=[row.geometry.y, row.geometry.x],
+        radius=4,
+        color='black',
+        fill=True,
+        fill_color='black',
+        fill_opacity=1,
+        popup=f'Competencia Score: {competencia_score}'
+    ).add_to(m)
+
+colormap.add_to(m)
+
+m.save('map_with_competencia_score.html')
+
+m
+
+heat_data = [[row.geometry.y, row.geometry.x, row['competencia_score']] for idx, row in puntaje.iterrows()]
+m_heat = folium.Map(location=map_center, zoom_start=12)
+HeatMap(heat_data).add_to(m_heat)
+
+m_heat.save('heat_mappuntaje.html')
+
+m_heat
